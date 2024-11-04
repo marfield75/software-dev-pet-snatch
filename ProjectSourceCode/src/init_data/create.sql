a2d4aaa83781edfc004dc7d96837267fdd65eb1f
Pradyumna Bangalore Sheshadri <prba4871@colorado.edu>
2024-11-04 08:20:18 -0700

    create tables done

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,1 +1,35 @@
-CREATE TABLE pets();
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username text,
+    first_name text, 
+    last_name text,
+    password text,
+    email text
+);
+
+CREATE TABLE pets(
+    id SERIAL PRIMARY KEY,
+    name text,
+    class text,
+    breed text,
+    eye_color text,
+    birthday DATE, 
+    location text,
+    bio text   
+);
+
+CREATE TABLE breeds(
+    id SERIAL PRIMARY KEY,
+    name text,
+    personality text 
+);
+
+CREATE TABLE users_to_pets(
+    user_id SMALLINT,
+    pet_id SMALLINT
+);
+
+CREATE TABLE pets_to_breeds(
+    pet_id SMALLINT,
+    breed_id SMALLINT
+);
