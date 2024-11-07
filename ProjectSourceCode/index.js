c7259b865187cc133e9400aa10282d827e03e495
Adeline Horstman <adho5467@colorado.edu>
2024-11-06 19:45:49 -0700

    adding barebones profile.hbs and get route

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -90,6 +90,10 @@ app.get('/home', (req, res) => {
     res.render('pages/home');
 });
 
+app.get('/home', (req, res) => {
+    res.render('pages/profile')
+});
+
 // POST route for handling registration form submission
 app.post('/register', async (req, res) => {
     const name = await req.body.username;
