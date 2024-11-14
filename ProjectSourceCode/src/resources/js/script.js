b202ddd9b188d0f16a716977210aad5d41bb7300
Shuoran Lin <shli1948@colorado.edu>
2024-11-14 15:40:55 -0700

    menu

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -1,20 +1,7 @@
-// 切换弹出框显示状态
-function toggleSearchModal() {
-    const modal = document.getElementById('search-modal');
-    modal.style.display = modal.style.display === 'flex' ? 'none' : 'flex';
-  }
-  
- 
-  window.onclick = function(event) {
-    const modal = document.getElementById('search-modal');
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  }
-  
-  // 切换搜索栏的显示状态
-function toggleSearchBar() {
-    const searchBar = document.getElementById('search-bar');
-    searchBar.classList.toggle('hidden');
-  }
+// 控制菜单显示和隐藏
+document.getElementById("searchDropdown").addEventListener("click", function (event) {
+    event.preventDefault(); // 防止默认行为
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.classList.toggle("show");
+  });
   
